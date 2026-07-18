9c6d4fd01295a512909c00e1930c6a71535259a0
Resioku <82930426+Resioku@users.noreply.github.com>
2026-07-18 19:36:01 -0400

    Update notes.js

--- notes.js
+++ notes.js
@@ -5,14 +5,14 @@
 const NOTES_TEXT = `
 - Prismatic is marked [WIP] and off by default in Filters. It technically can use a lot of non-Prismatic exotics, but that list isn't built out yet.
 
-- If you see any bugs or have any suggestions, let me know on Discord at Drift's End! https://discord.gg/QezssQZGCw
+- If you see any bugs or have any suggestions, let me know on Discord at **Drift's End**! https://discord.gg/QezssQZGCw
 `;
 
 // Same idea, but for a running changelog. This box scrolls on its own,
 // so feel free to just keep adding entries at the top (or bottom,
 // whichever you prefer) as things change.
 const PATCH_NOTES_TEXT = `
-7/18/2026
+**7/18/2026**
 - Added Filter Saving
 - Added Notes
 - Allowed Prismatic to pull from the Neutral exotics, still haven't added every exotic it synergizes with however
@@ -20,6 +20,6 @@ const PATCH_NOTES_TEXT = `
 - Added Exotic Armor Randomizing
 - Added Patch Notes
 
-7/17/2026
+**7/17/2026**
 - Creation
 `; 
